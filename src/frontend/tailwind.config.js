80a4e3a5f0df58d012d31f32cbd59c77fac8503f
root <root@DESKTOP-G8JH7CL>
2024-08-05 22:09:31 +0900

    updated frontend with icp

--- src/frontend/tailwind.config.js
+++ src/frontend/tailwind.config.js
@@ -1,8 +1,11 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  darkMode: 'class',
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    "/node_modules/react-tailwindcss-select/dist/index.esm.js"
+  ],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
-};
+  plugins: [],
+}
