1e4d5f2ad4e5ff5516a3d170dec2d86344342fc7
Tanguy <sardariuss@gmail.com>
2024-08-06 09:11:07 -0400

    Revert "Bring "ICP repository" front-end"

--- src/frontend/src/main.jsx
+++ src/frontend/src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { PrimeReactProvider, PrimeReactContext } from "primereact/api";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import "./index.scss";
+import store from "./store";
+import IIProvider from "./components/IIProvider";
+import Router from "./components/Router";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Provider store={store}>
+      <IIProvider>
+        <PrimeReactProvider>
+          <Router />
+        </PrimeReactProvider>
+      </IIProvider>
+    </Provider>
+  </React.StrictMode>
 );
--- src/frontend/tailwind.config.js
+++ src/frontend/tailwind.config.js
@@ -1,11 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}',
-    "/node_modules/react-tailwindcss-select/dist/index.esm.js"
-  ],
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [require("daisyui")],
+};
