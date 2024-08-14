43e04d478b6594edd2b68d1720665f6ae514cc72
sardariuss <sardariuss@gmail.com>
2024-08-14 07:49:57 -0400

    Reviewed front-end folder architecture

--- src/frontend/components/Footer.tsx
+++ src/frontend/components/Footer.tsx
@@ -1,18 +1,13 @@
 import React from 'react'
-import Logo from "./logo-new.svg"
+
 function Footer() {
   return (
-  
-
-<footer class="bg-white  shadow dark:bg-gray-900 ">
-    <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-     
+    <footer class="bg-white  shadow dark:bg-gray-900 ">
+      <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="#" class="hover:underline">BipQuantum</a>. All Rights Reserved.</span>
-    </div>
-</footer>
-
-
+      </div>
+    </footer>
   )
 }
 
