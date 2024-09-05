95b8f9f20e0509516b720abe41b5926481f54727
cyber1129 <>
2024-09-05 17:22:59 -0400

    Add copy button and ip view page

--- src/frontend/components/IPList.tsx
+++ src/frontend/components/IPList.tsx
@@ -1,64 +1,78 @@
-import { backendActor } from "./actors/BackendActor"
-import { intPropLicenseToString, intPropTypeToString } from "../utils/conversions"
-import IPPrice from "./IPPrice"
-import UserDetails from "./UserDetails"
-import IpOwner from "./IpOwner"
+import { Link } from "react-router-dom";
+
+import { backendActor } from "./actors/BackendActor";
+import {
+  intPropLicenseToString,
+  intPropTypeToString,
+} from "../utils/conversions";
+import IPPrice from "./IPPrice";
+import UserDetails from "./UserDetails";
+import IpOwner from "./IpOwner";
 
 const IPList = () => {
-
   const { data: entries } = backendActor.useQueryCall({
     functionName: "get_int_props",
-    args: [{
-      prev: [],
-      take: [BigInt(10)],
-    }],
-  })
+    args: [
+      {
+        prev: [],
+        take: [BigInt(10)],
+      },
+    ],
+  });
 
   const { call: buyIntProp } = backendActor.useUpdateCall({
-    functionName: "buy_int_prop"
-  })
+    functionName: "buy_int_prop",
+  });
 
   const triggerBuy = (intPropId: bigint) => {
-    buyIntProp([{token_id: intPropId}]).then((result) =>{
-      if (!result){
-        console.error("Failed to buy: undefined error")
+    buyIntProp([{ token_id: intPropId }]).then((result) => {
+      if (!result) {
+        console.error("Failed to buy: undefined error");
       } else {
-        if ('ok' in result) {
-          console.log("Buy succeeded")
+        if ("ok" in result) {
+          console.log("Buy succeeded");
         } else {
-          console.log("Buy failed")
+          console.log("Buy failed");
         }
-        console.log(result)
+        console.log(result);
       }
-    })
-  }
+    });
+  };
 
   return (
     <>
-      {
-        entries === undefined ? (
-          <div className="text-center text-white" style={{ 
-            padding: "100px"
-            }}>Loading...</div>
-        ) : 'err' in entries ? (
-          <div>
-            <h1>Error</h1>
-            <p>{entries.err}</p>
-          </div>
-        ) : entries.ok.length === 0 ? (
-          <div className="text-center text-black dark:text-white" style={{ 
-            padding: "100px"
-            }}>
-            Please add your First IP.
-          </div>
-        ) : (
-          <ul> {
-            entries.ok.map(([intPropId, intProp]) => ( 
-              <li
-                id="order_container"
-                className="w-full  m-12 bg-white dark:bg-gray-800 p-8 shadow-lg rounded-lg max-w-8xl my-4  mx-auto"
-                key={intPropId}
-              >
+      {entries === undefined ? (
+        <div
+          className="text-center text-white"
+          style={{
+            padding: "100px",
+          }}
+        >
+          Loading...
+        </div>
+      ) : "err" in entries ? (
+        <div>
+          <h1>Error</h1>
+          <p>{entries.err}</p>
+        </div>
+      ) : entries.ok.length === 0 ? (
+        <div
+          className="text-center text-black dark:text-white"
+          style={{
+            padding: "100px",
+          }}
+        >
+          Please add your First IP.
+        </div>
+      ) : (
+        <ul>
+          {entries.ok.map(([intPropId, intProp]) => (
+            <li
+              id="order_container"
+              className="w-full  m-12 bg-white dark:bg-gray-800 p-8 shadow-lg rounded-lg max-w-8xl my-4  mx-auto"
+              key={intPropId}
+            >
+              <Link to={`/ip/${intPropId.toString()}`}>
                 <div className="grid grid-cols-2 md:grid-cols-4 gap-4 p-4 rounded-lg bg-gray-200">
                   <div>
                     <div className="text-sm font-semibold text-black dark:text-white">
@@ -84,7 +98,9 @@ const IPList = () => {
                     </div>
                     <div className="text-lg font-bold text-black dark:text-white">
                       {" "}
-                      { (new Date(Number(intProp.creationDate))).toLocaleDateString() }{" "}
+                      {new Date(
+                        Number(intProp.creationDate)
+                      ).toLocaleDateString()}{" "}
                     </div>
                   </div>
                 </div>
@@ -103,31 +119,28 @@ const IPList = () => {
                         </h3>
                       </div>
                     </div>
-                  </div>  
-                  <div>
-                    Author
-                  </div>            
-                  <UserDetails principal={intProp.author} />
-                  <div>
-                    Owner
                   </div>
+                  <div>Author</div>
+                  <UserDetails principal={intProp.author} />
+                  <div>Owner</div>
                   <IpOwner intPropId={intPropId} />
                   <button
-                    onClick={() => { triggerBuy(intPropId) }}
+                    onClick={() => {
+                      triggerBuy(intPropId);
+                    }}
                     className="block text-white dark:text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                     type="button"
                   >
                     Buy
                   </button>
                 </div>
-              </li>
-            ))
-          } </ul>
-        )
-      }
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
     </>
-  )
+  );
+};
 
-}
-
-export default IPList
+export default IPList;
--- src/frontend/components/LoggedIn.tsx
+++ src/frontend/components/LoggedIn.tsx
@@ -1,25 +1,27 @@
-import { useAuth } from '@ic-reactor/react';
-import { backendActor } from './actors/BackendActor';
-import { Principal } from '@dfinity/principal';
-import Balance from "./Balance"
+import { useAuth } from "@ic-reactor/react";
+import { Principal } from "@dfinity/principal";
+import { Link } from "react-router-dom";
+
+import { backendActor } from "./actors/BackendActor";
+
+import Balance from "./Balance";
 
 type LoggedInProps = {
   toggleModal: () => void;
   toggleUserModal: () => void;
-  principal: Principal
-}
+  principal: Principal;
+};
 
-function LoggedIn({ toggleModal, toggleUserModal, principal } : LoggedInProps) {
-
+function LoggedIn({ toggleModal, toggleUserModal, principal }: LoggedInProps) {
   // TODO sardariuss 2024-SEP-03: remove log
-  console.log(principal.toText())
-  
+  console.log(principal.toText());
+
   const { logout } = useAuth({});
 
   const { data: user_account } = backendActor.useQueryCall({
     functionName: "get_user_account",
-    args: [{user: principal}]
-  })
+    args: [{ user: principal }],
+  });
 
   return (
     <div className="flex flex-row gap-x-3">
@@ -30,22 +32,66 @@ function LoggedIn({ toggleModal, toggleUserModal, principal } : LoggedInProps) {
       >
         Create New IP
       </button>
-      { user_account ?
-        <Balance account={user_account}/> : <></>
-      }
-      <button onClick={toggleUserModal} type="button" className="flex text-sm bg-gray-800  px-2 items-center rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
+      <Link
+        className="block text-white dark:text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        to="/myip"
+      >
+        My Ip
+      </Link>
+      {user_account ? <Balance account={user_account} /> : <></>}
+      <button
+        onClick={toggleUserModal}
+        type="button"
+        className="flex text-sm bg-gray-800  px-2 items-center rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+        id="user-menu-button"
+        aria-expanded="false"
+        data-dropdown-toggle="user-dropdown"
+        data-dropdown-placement="bottom"
+      >
         <span className="sr-only">Open user menu</span>
-        <svg className="w-6 h-6 text-white dark:text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-          <path fillRule="evenodd" d="M12 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4h-4Z" clipRule="evenodd"/>
+        <svg
+          className="w-6 h-6 text-white dark:text-gray-800 dark:text-white"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          fill="currentColor"
+          viewBox="0 0 24 24"
+        >
+          <path
+            fillRule="evenodd"
+            d="M12 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4h-4Z"
+            clipRule="evenodd"
+          />
         </svg>
       </button>
-      <button onClick={() => { logout() }}  type="button" className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
-        <svg className="w-6 h-6 text-white dark:text-gray-800 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 12H4m12 0-4 4m4-4-4-4m3-4h2a3 3 0 0 1 3 3v10a3 3 0 0 1-3 3h-2"/>
+      <button
+        onClick={() => {
+          logout();
+        }}
+        type="button"
+        className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
+      >
+        <svg
+          className="w-6 h-6 text-white dark:text-gray-800 "
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <path
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M16 12H4m12 0-4 4m4-4-4-4m3-4h2a3 3 0 0 1 3 3v10a3 3 0 0 1-3 3h-2"
+          />
         </svg>
       </button>
     </div>
-    )
+  );
 }
 
-export default LoggedIn
+export default LoggedIn;
